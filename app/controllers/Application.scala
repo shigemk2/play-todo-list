39211ecd6568dc4de9d6513aa14edb0b4d63b662
shigemk2 <i.am.shige@gmail.com>
2014-06-12 00:00:51 +0900

    Add TODO

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,14 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    // Ok(views.html.index("Your new application is ready."))
+    Ok("Hello world")
   }
+
+  def tasks = TODO
+
+  def newTask = TODO
+
+  def deleteTask(id: Long) = TODO
 
 }
